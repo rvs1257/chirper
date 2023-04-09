d90e78f314ffa5bc04ce4b69f5edfe2ac6cc09c4
rudis <rudi.steinbach@gmail.com>
2023-04-09 21:30:54 +0200

    Profile page with ssg helper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,18 @@ import { type AppProps, type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }: AppProps) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Chirper</title>
+        <meta name="description" content="💭" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ClerkProvider>
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,14 +1,10 @@
 import { z } from "zod";
 import { clerkClient } from "@clerk/nextjs/server"
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
-import type { User } from "@clerk/nextjs/dist/api";
 import { TRPCError } from "@trpc/server";
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
-
-const filterUserForClient = (user: User) => {
-  return { id: user.id, username: user.username, profileImageUrl: user.profileImageUrl }
-}
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
